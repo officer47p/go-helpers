c40c4e28939a54395de00ff83717d42466ee8730
Parsa Hosseini <officer47p@gmail.com>
2023-11-22 10:46:50 +0330

    Add convert float functions

--- helpers.go
+++ helpers.go
@@ -1,7 +1,14 @@
 package gohelpers
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func FloatToString(n float64) string {
 	return fmt.Sprintf("%g", n)
+}
+
+func StringToFloat(n string) (float64, error) {
+	return strconv.ParseFloat(n, 64)
 }
--- helpers_test.go
+++ helpers_test.go
@@ -3,10 +3,40 @@ package gohelpers
 import "testing"
 
 func TestFloatToString(t *testing.T) {
-	want := "0.222"
-	got := FloatToString(0.222)
+	tests := []struct {
+		name  string
+		input float64
+		want  string
+	}{
+		{name: "only decimals", input: 0.222, want: "0.222"},
+		{name: "decimals with integer", input: 12.23003, want: "12.23003"},
+	}
 
-	if got != want {
-		t.Errorf("got %s, but want %s", got, want)
+	for _, test := range tests {
+		got := FloatToString(test.input)
+		if got != test.want {
+			t.Errorf("%s: expected: %v, got: %v", test.name, test.want, got)
+		}
+	}
+}
+
+func TestStringToFloat(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  float64
+	}{
+		{name: "only decimals", input: "0.222", want: 0.222},
+		{name: "decimals with integer", input: "12.23003", want: 12.23003},
+	}
+
+	for _, test := range tests {
+		got, err := StringToFloat(test.input)
+		if err != nil {
+			t.Fatalf("error while converting string to float. Err: %s", err.Error())
+		}
+		if got != test.want {
+			t.Errorf("%s: expected: %v, got: %v", test.name, test.want, got)
+		}
 	}
 }
